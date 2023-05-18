388382e33e58e80ecd4c5141e9ebb3228f9dd329
flowerlyyy <gullushammadli@gmail.com>
2023-05-18 23:46:04 +0400

    Project 4 is added

--- project1.js
+++ project1.js
@@ -73,8 +73,33 @@ function resetfunction () {
    countNumberEl.style.color = 'black';
 }
 
+//PROJECT 4
+const leftBtn = document.querySelector(".btn-left");
+const rightBtn = document.querySelector(".btn-right");
+const buttonsEl = document.querySelector(".button");
+const sliderEl = document.querySelector(".slider");
 
+let imageNo = ["1","2","3","4","5","6"];
 
+let number = 0;
+
+leftBtn.addEventListener("click",leftfunction);
+rightBtn.addEventListener("click",rightfunction);
+
+function rightfunction(){
+    number++;
+    if(number>imageNo.length-1){
+        number=0;
+    }
+    sliderEl.style.background = `url("images/${imageNo[number]}.png")`;
+}
+function leftfunction(){
+    number--;
+    if(number<0){
+        number = imageNo.length-1;
+    }
+    sliderEl.style.background = `url("images/${imageNo[number]}.png")`;
+}
 
 
 
